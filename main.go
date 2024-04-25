d6d9cf4960f2d11a37c8fe213071cde20b73a1cf
pigeatgarlic <huyhoangdo0205@gmail.com>
2024-04-25 09:46:55 +0000

    simple watcher

--- main.go
+++ main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	"encoding/json"
+	// "encoding/json"
 	"fmt"
 	"io"
 	"io/fs"
 	"net"
-	"net/http"
+
+	// "net/http"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
 	"time"
-
-	"github.com/fsnotify/fsnotify"
 )
 
 func GetFreePort() (port int, err error) {
@@ -27,51 +26,46 @@ func GetFreePort() (port int, err error) {
 	return 0, err
 }
 
-func UploadInfoFunction(update_host, msg string) {
-	raw, _ := json.Marshal(struct {
-		Timestamp string `json:"timestamp"`
-		Level     string `json:"level"`
-		Source    string `json:"source"`
-		Data      string `json:"data"`
-	}{
-		Data:      msg,
-		Source:    "unknown",
-		Level:     "unknown",
-		Timestamp: time.Now().Format(time.RFC3339),
-	})
+func UploadInfoFunction(update_host, source, timestamp, msg string) {
+	// raw, _ := json.Marshal(struct {
+	// 	Timestamp string `json:"timestamp"`
+	// 	Level     string `json:"level"`
+	// 	Source    string `json:"source"`
+	// 	Data      string `json:"data"`
+	// }{
+	// 	Data:      msg,
+	// 	Source:    "unknown",
+	// 	Level:     "unknown",
+	// 	Timestamp: time.Now().Format(time.RFC3339),
+	// })
 
-	body, _ := json.Marshal(struct {
-		Data  []string `json:"data"`
-		Token string   `json:"token"`
-	}{
-		Data:  Encrypt(raw),
-		Token: "token",
-	})
+	// body, _ := json.Marshal(struct {
+	// 	Data []string `json:"data"`
+	// 	Token string `json:"token"`
+	// }{
+	// 	Data: Encrypt(raw),
+	// 	Token: "token",
+	// })
 
-	resp, err := http.Post(
-		fmt.Sprintf("%s/log_upload_encrypted", update_host),
-		"application/json",
-		strings.NewReader(string(body)),
-	)
-	if err != nil {
-		panic(err)
-	}
+	// resp, err := http.Post(
+	// 	fmt.Sprintf("%s/log_upload_encrypted", update_host),
+	// 	"application/json",
+	// 	strings.NewReader(string(body)),
+	// )
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	resp_body, _ := io.ReadAll(resp.Body)
-	if resp.StatusCode != 200 {
-		panic(fmt.Errorf(string(resp_body)))
-	}
+	// resp_body, _ := io.ReadAll(resp.Body)
+	// if resp.StatusCode != 200 {
+	// 	panic(fmt.Errorf(string(resp_body)))
+	// }
 
-	fmt.Println(msg)
+	fmt.Printf("%s %s %s\n", source, timestamp, msg)
 }
 
 func captureLog(update_host string, proc *exec.Cmd) {
 	stdout, err := proc.StdoutPipe()
-	if err != nil {
-		fmt.Printf("%s\n", err.Error())
-		return
-	}
-	stderr, err := proc.StderrPipe()
 	if err != nil {
 		fmt.Printf("%s\n", err.Error())
 		return
@@ -88,36 +82,47 @@ func captureLog(update_host string, proc *exec.Cmd) {
 			}
 			for _, lines := range strings.Split(string(buffer[:size]), "\n") {
 				for _, line := range strings.Split(lines, "\t") {
-					if len(line) == 0{
+					if len(line) == 0 {
 						continue
 					}
 
 					timestamp := time.Now().Format(time.DateTime)
-					UploadInfoFunction(update_host, fmt.Sprintf("%s : %s", timestamp, line))
+					UploadInfoFunction(update_host, proc.Args[3], timestamp, line)
 				}
 			}
 
 		}
 	}()
-	go func() {
-		for {
-			size, err := stderr.Read(buffer)
-			if err != nil {
-				end <- err
-			}
-			UploadInfoFunction(update_host, string(buffer[:size]))
-		}
-	}()
 	fmt.Printf("file watcher self closed %s\n", (<-end).Error())
 }
 
+func IterateFolder(path string) []string {
+	watching_files := []string{}
+	err := filepath.Walk(path, func(path string, info fs.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("failed to initialize file watcher %s\n", err.Error())
+			return err
+		} else if info.IsDir() {
+			return nil
+		}
+
+		watching_files = append(watching_files, path)
+		return nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	return watching_files
+}
+
 func main() {
-	watch_folder := "./data/"
+	watch_folder := "/data/ecosystem/app_data/t2d_logs"
 	manifest := "./.image-info"
 	update_host, found := os.LookupEnv("BIOTURING_T2D_HOST")
 	if !found {
 		fmt.Println("bioturing update host not found") // TODO
-		update_host = "http://localhost:8080"
+		update_host = "http://localhost:60000"
 	}
 
 	go func() {
@@ -134,38 +139,15 @@ func main() {
 				panic(err)
 			}
 
-			UploadInfoFunction(update_host, string(data))
+			// UploadInfoFunction(update_host, string(data))
+			_ = data
 			time.Sleep(time.Minute)
 		}
 	}()
 
 	go func() {
-		watcher, err := fsnotify.NewWatcher()
-		if err != nil {
-			fmt.Printf("failed to initialize file watcher %s\n", err.Error())
-			return
-		}
-		defer watcher.Close()
-
 		watch_process := map[string]*os.Process{}
-		watching_files := []string{}
-		watcher.Add(watch_folder)
-		err = filepath.Walk(watch_folder, func(path string, info fs.FileInfo, err error) error {
-			if err != nil {
-				fmt.Printf("failed to initialize file watcher %s\n", err.Error())
-				return err
-			} else if info.IsDir() {
-				return nil
-			}
-
-			watching_files = append(watching_files, path)
-			return nil
-		})
-
-		fmt.Printf("watching files %v\n", watching_files)
-		if err != nil {
-			fmt.Printf("failed to initialize file watcher %s\n", err.Error())
-		}
+		watching_files := IterateFolder(watch_folder)
 
 		go func() {
 			for {
@@ -209,7 +191,8 @@ func main() {
 						continue
 					}
 
-					cmd := exec.Command("tail","-n0", "-f", shouldwatch_file)
+					fmt.Printf("watching file %s\n", shouldwatch_file)
+					cmd := exec.Command("tail", "-n0", "-f", shouldwatch_file)
 					go captureLog(update_host, cmd)
 					watch_process[shouldwatch_file] = cmd.Process
 				}
@@ -219,42 +202,8 @@ func main() {
 		}()
 
 		for {
-			select {
-			case event, ok := <-watcher.Events:
-				if !ok {
-					fmt.Printf("error watch event\n")
-				}
-
-				if event.Op == fsnotify.Create {
-					if f, err := os.Open(event.Name); err != nil {
-						fmt.Printf("new watching file: %v\n", event.Name)
-					} else {
-						if info, err := f.Stat(); err == nil {
-							if info.IsDir() {
-								watcher.Add(event.Name)
-							} else {
-								watching_files = append(watching_files, event.Name)
-								fmt.Printf("new watching file: %v\n", event.Name)
-							}
-						}
-					}
-				} else if event.Op == fsnotify.Remove {
-					temp := []string{}
-					for _, file := range watching_files {
-						if event.Name == file {
-							continue
-						}
-						temp = append(temp, file)
-					}
-					watching_files = temp
-				}
-			case event, ok := <-watcher.Errors:
-				if !ok {
-					fmt.Printf("error watch error\n")
-				}
-
-				fmt.Printf("error watch file : %s\n", event.Error())
-			}
+			watching_files = IterateFolder(watch_folder)
+			time.Sleep(time.Second)
 		}
 	}()
 
